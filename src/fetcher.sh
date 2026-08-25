df49b8dc57be28f3e6a7ac2fa8f09af239d14923
RadioAddition <radioaddition@pm.me>
2026-08-25 15:24:37 -0400

    fix my sed expression

--- src/fetcher.sh
+++ src/fetcher.sh
@@ -12,7 +12,7 @@ function get_latest_version() {
       awk -F'\t' '{print $2}' |
       grep -E 'refs/tags/' |
       sed 's/refs\/tags\///' |
-      sed 's/\^\{\}//' |
+      sed 's/\^{}//' |
       sort -V |
       tail -n1
   )
